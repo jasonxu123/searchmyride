7c1bd2975cf0e0ed4d08283b6b4b3ef6e3e2e32d
Jason Xu <xu.jason.jx@gmail.com>
2017-10-07 06:57:05 -0700

    add more post data: sender and post's link

--- search.py
+++ search.py
@@ -5,11 +5,11 @@ from pprint import pprint
 # 60 day token, no need to refresh periodically
 token = '1506656972703387|XWNdzbKLOrqwT61ylrtH_kQZgkk'
 graph = facebook.GraphAPI(access_token=token, version=2.10)
-page = graph.get_object(id='459693560717220/feed', limit="100")
+page = graph.get_object(id='459693560717220/feed', limit="100", fields="updated_time, message, from")
 
 all_info = {}
 for i, post in enumerate(page["data"]):
-	all_info[i] = (post["message"], post["updated_time"])
+	all_info[i] = (post["message"], post["updated_time"], post["from"]["name"], "http://www.facebook.com/" + post["id"])
 # the u' before strings = unicode
 pprint(all_info)
 
