f4a967d7f4aa66b69ef4ad62d6216fec12b07284
Jason Xu <jasonxu123@airbears2-10-142-155-10.airbears2.1918.berkeley.edu>
2017-10-07 04:29:23 -0700

    add basic post retrieval from FB

---
+++ search.py
@@ -0,0 +1,12 @@
+import facebook
+import json
+from pprint import pprint
+# get token from Graph Explorer API, give user access options by checkmarks
+token = 'EAACEdEose0cBABOZA77ZC1JNNu7ptgEdZCaSwqnqG0NwZB2uYZADNwHxKCmDmZAMERMfRINxSjdYgys0xnnWpbCLu5ke2hxAoqYiBeDNuPL95WA0yvmhO67c4HWDZBkTlWUE4SoPQ5oyXb4ZA3GdfaBU9yTmZBGYdbjxfyZAeoAp9GM7VR0wGXFx19hy3pWf4moloZD'
+graph = facebook.GraphAPI(access_token=token, version=2.10)
+page = graph.get_object(id='459693560717220/feed')
+
+all_info = {}
+for i, post in enumerate(page["data"]):
+	all_info[i] = (post["message"], post["updated_time"])
+pprint(all_info)
