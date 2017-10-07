322fed86255ffab090f3675cbaef3a22bb7e708c
Jason Xu <xu.jason.jx@gmail.com>
2017-10-07 10:55:50 -0700

    classify routes as driving or riding requests

--- parse.py
+++ parse.py
@@ -9,16 +9,18 @@ def load_json(filename):
     return data
 
 def getdata(message):
-    data = load_json('locations.json')
+    data = load_json('keywords.json')
+
+    startFound = False
+    destFound = False
     searchObj = re.search(r'([a-zA-Z ]*) *(?:-+>*| to ) *([a-zA-Z ]*)', message, re.M|re.I)
     # searchObj = re.search(r'([a-zA-Z]*) (?:to) *([a-zA-Z]*)', message, re.M|re.I)
     if searchObj:
         start = searchObj.group(1)
         dest = searchObj.group(2)
-        startFound = False
-        destFound = False
+
         # print start.lower(), dest.lower()
-        for k, v in data.items():
+        for k, v in data["locations"].items():
             # print k, v
             # if start.lower() in v:
             for loc in v:
@@ -45,14 +47,29 @@ def getdata(message):
         # print "Nothing found! for " + message
         start = None
         dest = None
-    return start, dest
+
+    # returns if driver route (default = False), and start and destination
+    if destFound:
+        lower_mess = message.lower()
+        for keyword in data["driving-keys"]:
+            if keyword in lower_mess:
+                return True, start, dest
+        for keyword in data["riding-keys"]:
+            if keyword in lower_mess:
+                return False, start, dest
+        for unclear_word in data["ambiguous-keys"]:
+            if unclear_word in lower_mess:
+                if '?' in lower_mess:
+                    return False, start, dest
+                return True, start, dest
+    return False, start, dest
 
 def main():
     # get token from Graph Explorer API, give user access options by checkmarks
-    # token = 'EAACEdEose0cBABOZA77ZC1JNNu7ptgEdZCaSwqnqG0NwZB2uYZADNwHxKCmDmZAMERMfRINxSjdYgys0xnnWpbCLu5ke2hxAoqYiBeDNuPL95WA0yvmhO67c4HWDZBkTlWUE4SoPQ5oyXb4ZA3GdfaBU9yTmZBGYdbjxfyZAeoAp9GM7VR0wGXFx19hy3pWf4moloZD'
     token = '1506656972703387|XWNdzbKLOrqwT61ylrtH_kQZgkk'
     graph = facebook.GraphAPI(access_token=token, version=2.10)
     page = graph.get_object(id='459693560717220/feed', limit="100", fields="updated_time, message, from")
+    routeTypeList = []
     startList = []
     destList = []
     timeList = []
@@ -61,11 +78,12 @@ def main():
 
     # all_info = {}
     for i, post in enumerate(page["data"]):
-        start, dest = getdata(post["message"])
+        is_driving_route, start, dest = getdata(post["message"])
         time = post["updated_time"]
         name = post["from"]["name"]
         link = "http://www.facebook.com/" + post["id"] 
         if start and dest:
+            routeTypeList.append("Driving" if is_driving_route else "Riding")
             startList.append(start)
             destList.append(dest)
             timeList.append(time)
@@ -73,7 +91,7 @@ def main():
             linkList.append(link)
 
     for i in range(len(startList)):
-        print startList[i], destList[i], timeList[i], nameList[i], linkList[i]
+        print routeTypeList[i], startList[i], destList[i], timeList[i], nameList[i], linkList[i]
         # print i, post
     #     all_info[i] = (post["message"], post["updated_time"])
 
